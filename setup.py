36de57bbe89388f995032792641b99ee1498be5c
jcc <jcc@med-nodered-10.jcc-media.local>
2022-06-24 17:16:28 -0400

    v0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='libpcocheckin',
-    version='0.2',
+    version='0.5',
     author='David Hudson',
     author_email='unpublished',
     description='Functions for working with Planning Center Online Check-in API',
